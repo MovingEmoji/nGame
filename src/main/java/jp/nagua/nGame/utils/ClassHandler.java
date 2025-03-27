00fb4cfa8be6467e42de5f9de976b53d8f529424
MovingEmoji <mcnana814@gmail.com>
2025-03-27 14:52:39 +0900

    変数名を正しい名前へ変更

--- src/main/java/jp/nagua/nGame/utils/ClassHandler.java
+++ src/main/java/jp/nagua/nGame/utils/ClassHandler.java
@@ -11,8 +11,8 @@ public class ClassHandler {
     public static void loadClasses(String packageName) {
         try {
             Set<Class<?>> classes = ClassPath.from(Main.getPluginClassLoader()).getTopLevelClasses(packageName).stream().map(info -> info.load()).collect(Collectors.toSet());
-            for(Class<?> commandclass : classes) {
-                Class.forName(commandclass.getName(), true, Main.getPluginClassLoader());
+            for(Class<?> loadclass : classes) {
+                Class.forName(loadclass.getName(), true, Main.getPluginClassLoader());
             }
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
