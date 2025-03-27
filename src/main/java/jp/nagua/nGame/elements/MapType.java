c4a491862b842035aabd050be24c72b31ccdd16e
MovingEmoji <mcnana814@gmail.com>
2025-03-27 14:53:57 +0900

    マップタイプを取り出しやすく

---
+++ src/main/java/jp/nagua/nGame/elements/MapType.java
@@ -0,0 +1,9 @@
+package jp.nagua.nGame.elements;
+
+public class MapType {
+    public static int NORMAL = 0;
+    public static int SUMO = 1;
+    public static int VANILLA = 2;
+    public static int SPLEEF = 3;
+    
+}
