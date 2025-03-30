0a5a09c96b74474365a1416fb29f5670a3458ab0
MovingEmoji <mcnana814@gmail.com>
2025-03-31 00:58:26 +0900

    インベントリコマンドの実装

---
+++ src/main/java/jp/nagua/npractice/commands/InventoryCommand.java
@@ -0,0 +1,48 @@
+package jp.nagua.npractice.commands;
+
+import jp.nagua.npractice.Main;
+import jp.nagua.npractice.elements.SerializedInventory;
+import jp.nagua.npractice.utils.DataHandler;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class InventoryCommand implements CommandExecutor {
+
+    static {
+        Main.getPlugin().getCommand("inventory").setExecutor(new InventoryCommand());
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player) {
+            if(strings.length == 0) {
+                commandSender.sendMessage(ChatColor.RED + "Please type Function[load/save/delete]");
+                return true;
+            }
+            if(strings.length >= 1) {
+                switch (strings[0]) {
+                    case "load":
+                        SerializedInventory.loadInventory((Player) commandSender, (SerializedInventory) DataHandler.getCommonDataFromDefault(strings[1] + "Inventory"));
+                        commandSender.sendMessage(ChatColor.GREEN + "Load inventory from " + strings[1]);
+                        break;
+                    case "save":
+                        DataHandler.putCommonDataToDefault(strings[1] + "Inventory", new SerializedInventory(strings[1], ((Player) commandSender).getInventory()));
+                        commandSender.sendMessage(ChatColor.GREEN + "Save inventory with " + strings[1]);
+                        break;
+                    case "delete":
+                        DataHandler.deleteCommonDataFromDefault(strings[1] + "Inventory");
+                        commandSender.sendMessage(ChatColor.RED + "Delete " + strings[1] + " inventory ");
+                        break;
+                    default:
+                        commandSender.sendMessage(ChatColor.RED + "Please type correct Function[load/save/delete]");
+                        break;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
