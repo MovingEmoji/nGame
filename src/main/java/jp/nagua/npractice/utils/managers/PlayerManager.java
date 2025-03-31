38dcc9aefad63b4c197a90d2e715e2f449626dc0
MovingEmoji <mcnana814@gmail.com>
2025-03-31 20:38:08 +0900

    プレイヤーをキューに参加する処理

--- src/main/java/jp/nagua/npractice/utils/managers/PlayerManager.java
+++ src/main/java/jp/nagua/npractice/utils/managers/PlayerManager.java
@@ -1,8 +1,13 @@
 package jp.nagua.npractice.utils.managers;
 
 import jp.nagua.npractice.elements.FixedLocation;
+import jp.nagua.npractice.elements.PracticeKit;
+import jp.nagua.npractice.elements.Queue;
 import jp.nagua.npractice.elements.serializes.SerializedInventory;
+import jp.nagua.npractice.types.QueueType;
 import jp.nagua.npractice.utils.handlers.DataHandler;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public class PlayerManager {
@@ -12,6 +17,27 @@ public class PlayerManager {
         }
         if(DataHandler.getCommonDataFromDefault("Inventory-Lobby") != null) {
             SerializedInventory.loadInventory(player, (SerializedInventory) DataHandler.getCommonDataFromDefault("Inventory-Lobby"));
+        }
+    }
+
+    public static void joinQueue(Player player, PracticeKit kit, QueueType type) {
+        QueueManager.addPlayerToQueue(kit, type, player);
+        player.closeInventory();
+        if(DataHandler.getCommonDataFromDefault("Inventory-Queue") != null) {
+            SerializedInventory.loadInventory(player, (SerializedInventory) DataHandler.getCommonDataFromDefault("Inventory-Queue"));
+        } else {
+            player.getInventory().clear();
+        }
+        player.sendMessage(ChatColor.GREEN + "You joined the " + type.getType() + " " + kit.getName() + " queue");
+    }
+
+    public static void leaveQueue(Player player) {
+        if(QueueManager.getPlayerQueue(player) != null) {
+            String kit_name = QueueManager.getPlayerQueue(player).getKit().getName();
+            String type = QueueManager.getPlayerQueue(player).getQueueType().getType();
+            QueueManager.removePlayerFromQueue(player);
+            player.sendMessage(ChatColor.RED + "You left the " + type + " " + kit_name + " queue");
+            initializePlayer(player);
         }
     }
 }
